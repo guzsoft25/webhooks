6ac82b8b3ce542cb9b24f5c60a0ad3e835f90edf
MARIO GUZMAN <guzsoft25@gmail.com>
2025-10-27 20:27:23 -0400

    Project creation and Domain creation

---
+++ Webhooks.Practice/Webhooks.Practice.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using Webhooks.Practice.Domain.Enums;
+
+namespace Webhooks.Practice.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public decimal Price { get; private set; }
+        public Status Status { get; private set; } = Status.ACTIVE;
+        public string? Image { get; private set; }
+        public DateTime?  UpdateAt { get; private set; }
+
+        
+    }
+}
---
+++ Webhooks.Practice/Webhooks.Practice.Domain/Entities/WebhookEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Webhooks.Practice.Domain.Entities
+{
+    public class WebhookEvent
+    {
+        public int Id { get; set; }
+        public string EventType { get; set; } = string.Empty;
+        public string Payload { get; set; } = string.Empty;
+        public DateTime ReceivedAt { get; set; } = DateTime.UtcNow;
+    }
+}
